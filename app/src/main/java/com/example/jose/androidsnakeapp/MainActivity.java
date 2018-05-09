5d1486fecb0b29e7dc1f01113d7a62e3fb9c899f
Jose <joseeduardocorella@gmail.com>
2018-05-08 17:14:34 -0700

    Swipe function added

--- app/src/main/java/com/example/jose/androidsnakeapp/MainActivity.java
+++ app/src/main/java/com/example/jose/androidsnakeapp/MainActivity.java
@@ -3,19 +3,23 @@ package com.example.jose.androidsnakeapp;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.jose.androidsnakeapp.driver.GameDriver;
+import com.example.jose.androidsnakeapp.enumerators.Direction;
 import com.example.jose.androidsnakeapp.enumerators.GameCondition;
 import com.example.jose.androidsnakeapp.structure.SnakeStructure;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
     private GameDriver gameDriver;
     private SnakeStructure snakeStructure;
 
     private final Handler handler = new Handler();
-    private final long delay= 150;
+    private final long delay= 200;
 
+    private float prevXcoord, prevYcoord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         gameDriver.beginGame();
 
         snakeStructure = (SnakeStructure) findViewById(R.id.SnakeStructure);
+        snakeStructure.setOnTouchListener(this);
         startHandler();
 
     }
@@ -48,5 +53,42 @@ public class MainActivity extends AppCompatActivity {
 
     private void GameLost() {
         Toast.makeText(this, "YOU LOST", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                prevXcoord = event.getX();
+                prevYcoord = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                float newXcoord = event.getX();
+                float newYcoord = event.getY();
+
+                if(Math.abs(newXcoord -prevXcoord) > Math.abs(newYcoord - prevYcoord)){
+                    // left right
+                    if (newXcoord > prevXcoord){
+                        //right
+                        gameDriver.updateDirection(Direction.Left);
+                    }else{
+                        //left
+                        gameDriver.updateDirection(Direction.Right);
+                    }
+                } else{
+                    if(newYcoord > prevYcoord){
+                        //up
+                        gameDriver.updateDirection(Direction.Down);
+                    } else{
+                        //down
+                        gameDriver.updateDirection(Direction.Up);
+                    }
+                }
+                break;
+
+
+
+        }
+        return true;
     }
 }
